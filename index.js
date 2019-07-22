906b392d8885e81530dad2d874daff2e1ad67862
hereiam123 <bdemaio1@gmail.com>
2019-07-21 20:19:24 -0400

    Nice recommended setup provided

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const express = require('express');
-const x = 1;
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('hello');
+  process.exit(1);
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+
+const obj = {
+  hello: function() {
+    console.log('hello');
+  }
+};
+
+obj.hello();
+
+class A {
+  foo() {
+    console.log('Hello World');
+    /* Expect error is 'this' is not called */
+  }
+}
+
+const car = new A();
+
+car.foo();
